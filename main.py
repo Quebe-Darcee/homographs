a484e3004c9e978cef0a7cd5102144ec0c0bb06b
Dax <daxtonwirth@gmail.com>
2022-10-15 01:57:32 -0700

    fixed test functionality

--- main.py
+++ main.py
@@ -4,10 +4,12 @@
     /home/user/secret/password.txt
 
 --Test Cases--
+(2-3 test cases to verify)
 
     -- ./ --
         Non-Homographs
         ./secret/password.txt
+        
 
         HomoGraphs
         ./password.txt
@@ -33,7 +35,7 @@
         passWord.txt
 
         Homographs
-        ~/Secret/../secret/password.txt
+        ~/secret/../secret/password.txt
 
     -- Start Folder --
         Non-Homographs
@@ -58,12 +60,28 @@ def manual_test(Full_File_Path):
     run_test(file_path1, file_path2, Full_File_Path)
 
 def test_cases(Full_File_Path):
-    # This needs to be worked on.....
-    file_path1 = "password.txt"
-    homographs = ["/~/random/../secret/password.txt", "password.txt", "~/Secret/../secret/password.txt", "~/secret/password", "../secret/password.txt", "./password.txt"]
-    non_homographs = ["../../random/foldername/password.txt", "~/folder/randomname/password.txt", "./secret/password.txt", "/home/user/Secret/Password.txt", "passWord.txt", "~/password.txt", "../user/secret.txt", "../../secret/password.txt", "./secret/password.txt"]
+    homographs = ["password.txt", "~/secret/password.txt", "./password.txt", "../secret/password.txt", "/~/random/../secret/password.txt", "/./user/secret/password.txt", "/./././user/secret/password.txt"]
+    non_homographs = ["../../random/foldername/password.txt", "~/folder/randomname/password.txt", "~/Secret/../secret/password.txt", "./secret/password.txt", "/home/user/Secret/Password.txt", "passWord.txt", "~/password.txt", "../user/secret.txt", "../../secret/password.txt", "./secret/password.txt"]
+    forbidden_file = "/home/user/secret/password.txt"
 
-    run_test(file_path1, homographs, Full_File_Path)
+    print("\nTESTING HOMOGRAPHS")
+    # Homographs
+    count = 1
+    for homograph in homographs:
+        print(f"\n{count}. {homograph}")
+        run_test(forbidden_file, homograph, Full_File_Path)
+        count+=1
+
+    print("\nTESTING NON-HOMOGRAPHS")
+    # Non-Homographs
+    count = 1
+    for non_homograph in non_homographs:
+        print(f"\n{count}. {non_homograph}")
+        run_test(forbidden_file, non_homograph, Full_File_Path)
+        count+=1
+
+    
+
 
 
 def run_test(file_path1, file_path2, Full_File_Path):
@@ -87,7 +105,7 @@ def tilde(file_path, File_Path):
     # Look for if ~ is in the file path
     if file_path.find("~") == 0:
         # Replace the ~ with the file path
-        file_path = File_Path + file_path.split("~")[1][1:]
+        file_path = File_Path.split("secret")[0] + file_path.split("~")[1][1:]
         return file_path
     else: # do nothing
         return file_path
@@ -102,10 +120,20 @@ def dot(file_path, Full_File_Path):
             file_path = Full_File_Path + file_path[2:]
     return(file_path)
 
+def two_dots(file_path, Full_File_Path):
+    # .. is start of file path
+    if file_path[:2] == "..":
+        # Split the file path to /home/user
+        modified_file_path = Full_File_Path.split("/secret")[0]
+        file_path = modified_file_path + file_path[2:]
+    return file_path
+
+
 def canonicalization(file_path, Full_File_Path):
     # Full File path starts normally with / (/home/user/secret/password.txt)
     if file_path[0] == "/":
         # test for if two dots are in middle (/home/user/../user/secret/)
+
 
         return file_path
 
@@ -116,6 +144,7 @@ def canonicalization(file_path, Full_File_Path):
     file_path = dot(file_path, Full_File_Path)
 
     # Two dots .. in any part of file (../secret/password.txt)
+    file_path = two_dots(file_path, Full_File_Path)
     """If (two dots)
         if (the two dots start the string)
             if (t)
@@ -132,10 +161,15 @@ def canonicalization(file_path, Full_File_Path):
     If /. is the first character, replace it with home/user/secret
     else just delete it because it refers to the current directory
     """
+    while file_path[:1] == "/.":
+        file_path = file_path.split("/.")[1]
+
+
     # Two // in file 
     
     # No / at start of file (password.txt)
-    #file_path = Full_File_Path + file_path1
+    if file_path[0] != "/":
+        file_path = Full_File_Path + file_path
     return file_path
 
 def getOneFolderUp(folderName):
